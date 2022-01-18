cbe8dc8f40279fbfbba4e72ce3dc548960ce6f2e
Cleverson Rocha <cleverson.rocha@minu.co>
2022-01-18 19:45:03 -0300

    Inicio do curso na Alura

---
+++ 01_secao_tipos_primitivos/02_type_string.js
@@ -0,0 +1,31 @@
+// tipo string
+
+const texto1 = 'Olá, mundo!';
+const texto2 = "Olá, mundo!";
+const senha = 'senhaSuperSegura456!';
+const stringDeNumeros = '3567';
+const meuNome = 'Cleverson';
+
+const citacao = 'O Leo disse "oi"!';
+const citacao2 = "O Leo disse 'oi'!";
+
+//concatenacao (+)
+
+const citacao3 = 'Meu nome é ';
+
+console.log(citacao3 + meuNome);
+
+//template string OU template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const senha2 = "minhaSenha123"
+console.log(senha2.length) // 13 caracteres
