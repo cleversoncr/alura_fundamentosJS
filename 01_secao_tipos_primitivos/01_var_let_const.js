6f29a1f2adfc4cb6ab8d6e9d18b7e998a825db87
Cleverson Rocha <cleverson.rocha@minu.co>
2022-01-18 22:24:20 -0300

    Declarando variaveis

---
+++ 01_secao_tipos_primitivos/01_var_let_const.js
@@ -0,0 +1,45 @@
+//var
+
+
+/*
+No momendo da execução do código, o motor do JS faz a leiturae encontra todas as variáveis
+e aquelas que não foram declaradas, ele cataloga como VAR.
+*/
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento; 
+// console.log(area);
+// var area;
+
+//let
+
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retangulo') {
+//   area = altura * comprimento;
+// } else {
+//   area = (altura * comprimento) / 2;
+// };
+
+// console.log(area);
+
+//const
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'retangulo') {
+  area = altura * comprimento;
+} else {
+  area = (altura * comprimento) / 2;
+};
+
+console.log(area);
+
