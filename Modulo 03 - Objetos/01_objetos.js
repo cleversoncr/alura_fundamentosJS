a99b9cc4f19404ccdcae48b652e8a7a66a63c20e
Cleverson Rocha <cleverson.rocha@minu.co>
2022-01-24 13:59:35 -0300

    Fim do modulo Objetos

--- Modulo 03 - Objetos/01_objetos.js
+++
@@ -1,9 +0,0 @@
-const listaCpfs = ['12345678901', '21234534565', '25481254789'];
-
-const cliente = {
-  nome: 'Cleverson',
-  idade: 43,
-  cpf: '12548756254',
-  email: 'cleversoncr@gmail.com'
-
-};
